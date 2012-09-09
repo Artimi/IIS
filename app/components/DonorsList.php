7aa0ba08f236493d9c0cf6be1f1f5fa344dc2acc
xsebek02 <petrsebek1@gmail.com>
2012-09-09 11:34:33 +0200

    Moved Detail to presenter, added base of invitations

--- app/components/DonorsList.php
+++ app/components/DonorsList.php
@@ -9,28 +9,27 @@
 namespace BloodCenter;
 
 use Nette;
-use Nette\Application\UI\Form;
 
 class DonorsListControl extends Nette\Application\UI\Control
 {
 
     /** @var \Nette\Database\Table\Selection */
-    private $donors;
+    private $donor;
     private $station;
 
-    public function __construct(\BloodCenter\Donor $donors,
+    public function __construct(\BloodCenter\Donor $donor,
                                 \BloodCenter\Station $station)
     {
         parent::__construct();
-        $this->donors = $donors;
+        $this->donor = $donor;
         $this->station = $station;
     }
 
     public function render()
     {
         $this->template->setFile(__DIR__ . '/DonorsList.latte');
-        $this['paginator']->paginator->itemCount = $this->donors->getCount();
-        $this->template->donors = $this->donors
+        $this['paginator']->paginator->itemCount = $this->donor->getCount();
+        $this->template->donors = $this->donor
             ->findOffset($this['paginator']->paginator->offset, $this['paginator']->paginator->itemsPerPage);
         $this->template->render();
     }
@@ -41,53 +40,5 @@ class DonorsListControl extends Nette\Application\UI\Control
         $vp->paginator->itemsPerPage = 4;
         return $vp;
     }
-
-    public function handleDetail($id)
-    {
-        $defaults = $this->donors->findOneBy(array('id' => $id));
-        $this['detail']->defaults = $defaults;
-    }
-
-    public function createComponentDetail($name)
-    {
-        $form = new Form($this, $name);
-        $form->addText('id', 'ID:')
-            ->setRequired()
-            ->setAttribute('readonly');
-        $form->addText('nick', 'Nick:')
-            ->setRequired()
-            ->setAttribute('readonly');
-        $form->addText('name', 'Name:'
-            )->setRequired();
-        $form->addText('surname', 'Surname:')
-            ->setRequired();
-        $form->addText('postal_code', 'Postal code:');
-        $form->addText('city', 'City:');
-        $form->addText('street', 'Street:');
-        $form->addText('phone', 'Phone:');
-        $form->addText('email', 'Email:');
-        $form->addText('blood_type', 'Blood type:'
-            )->setRequired();
-        $form->addText('national_id', 'National ID:')
-            ->setRequired();
-        $form->addRadioList('active', 'Active:',array(0 => 'inactive', 1 => 'active'))
-            ->setRequired();
-        $stationNames = $this->station->getStationNames();
-        $form->addSelect('pref_station','Preferred station:', $stationNames);
-        $form->addTextArea('note', 'Note:');
-        $form->addSubmit('edit', 'Edit');
-        $form->onSuccess[] = callback($this, 'detailEdited');
-        if (isset($form->defaults))
-            $form->setDefaults($form->defaults);
-        return $form;
-    }
-
-    public function detailEdited(Form $form)
-    {
-        $values = $form->getValues();
-        $this->donors->update($values, $values['id']);
-        $this->flashMessage('Donor ' . $values['name'] . ' ' . $values['surname'] . ' (' . $values['nick'] . ') was edited.');
-    }
-
 }
 
--- app/presenters/DonorPresenter.php
+++ app/presenters/DonorPresenter.php
@@ -1,5 +1,7 @@
 <?php
 
+use Nette\Application\UI\Form;
+
 /**
  * Description of DonorPresenter
  *
@@ -10,6 +12,7 @@ class DonorPresenter extends BasePresenter
 
     private $donor;
     private $station;
+    private $invitation;
 
     public function startup()
     {
@@ -22,6 +25,7 @@ class DonorPresenter extends BasePresenter
 
         $this->donor = $this->context->donor;
         $this->station = $this->context->station;
+        $this->invitation = $this->context->invitation;
     }
 
     public function renderDefault()
@@ -39,5 +43,56 @@ class DonorPresenter extends BasePresenter
         return new BloodCenter\DonorsListControl($this->donor, $this->station);
     }
 
+    public function renderDetail($id)
+    {
+        $defaults = $this->donor->findOneBy(array('id' => $id));
+        $this['detail']->defaults = $defaults;
+    }
+
+    public function createComponentDetail($name)
+    {
+        $form = new Form($this, $name);
+        $form->addText('id', 'ID:')
+            ->setRequired()
+            ->setAttribute('readonly');
+        $form->addText('nick', 'Nick:')
+            ->setRequired()
+            ->setAttribute('readonly');
+        $form->addText('name', 'Name:'
+        )->setRequired();
+        $form->addText('surname', 'Surname:')
+            ->setRequired();
+        $form->addText('postal_code', 'Postal code:');
+        $form->addText('city', 'City:');
+        $form->addText('street', 'Street:');
+        $form->addText('phone', 'Phone:');
+        $form->addText('email', 'Email:');
+        $form->addText('blood_type', 'Blood type:'
+        )->setRequired();
+        $form->addText('national_id', 'National ID:')
+            ->setRequired();
+        $form->addRadioList('active', 'Active:', array(0 => 'inactive', 1 => 'active'))
+            ->setRequired();
+        $stationNames = $this->station->getStationNames();
+        $form->addSelect('pref_station', 'Preferred station:', $stationNames);
+        $form->addTextArea('note', 'Note:');
+        $form->addSubmit('edit', 'Edit');
+        $form->onSuccess[] = callback($this, 'detailEdited');
+        if (isset($form->defaults))
+            $form->setDefaults($form->defaults);
+        return $form;
+    }
+
+    public function detailEdited(Form $form)
+    {
+        $values = $form->getValues();
+        $this->donor->update($values, $values['id']);
+        $this->flashMessage('Donor ' . $values['name'] . ' ' . $values['surname'] . ' (' . $values['nick'] . ') was edited.');
+    }
+    
+    public function renderInvitation($donorid)
+    {
+        $this->template->invitations = $this->invitation->findBy(array('donor'=>$donorid));
+    }
 }
 
